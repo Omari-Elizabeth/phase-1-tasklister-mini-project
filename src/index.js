1878ba6699c27bc4520f71bcd3875bcc0c953515
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-29 20:57:36 +0300

    trial

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+
+  const taskForm = document.getElementById("create-task-form")
+  taskForm.addEventListener('submit', addTask);
+  function addTask(e){
+    e.preventDefault()
+
+    const getInput = document.getElementById('new-task-description').value;
+     const lists = document.createElement('li');
+     lists.appendChild(document.createTextNode(getInput));
+
+     const deleteBtn = document.createElement('button');
+     deleteBtn.className = 'remove'
+     deleteBtn.appendChild(document.createTextNode('X'));
+     lists.appendChild(deleteBtn);
+
+     function removeTask(e){
+      if(e.target.className.contains('remove')){
+        lists.removeChild(li);
+      }
+     };
+
+  }
 });
