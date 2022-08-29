10e7254dc6599edf662d138e23918e560b3926f0
Omari-Elizabeth <elizabeth.omari@student.moringaschool.com>
2022-08-29 22:36:46 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -7,18 +7,25 @@ document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault()
 
     const getInput = document.getElementById('new-task-description').value;
-     const lists = document.createElement('li');
-     lists.appendChild(document.createTextNode(getInput));
+    const lists = document.createElement('li');
+    lists.appendChild(document.createTextNode(getInput));
 
-     const deleteBtn = document.createElement('button');
-     deleteBtn.className = 'remove'
-     deleteBtn.appendChild(document.createTextNode('X'));
-     lists.appendChild(deleteBtn);
+    document.querySelector('#tasks').appendChild(lists)
 
-     function removeTask(e){
-      if(e.target.className.contains('remove')){
-        lists.removeChild(li);
-      }
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'remove'
+    deleteBtn.appendChild(document.createTextNode('X'));
+    lists.appendChild(deleteBtn);
+
+    let removeButtons = document.querySelectorAll('.remove')
+
+    removeButtons.forEach(x => x.addEventListener('click', removeTask));
+
+    function removeTask(e){
+      // if(e.target.className.contains('remove')){
+      //   lists.removeChild(li);
+      // }
+      this.parentNode.remove();
      };
 
   }
